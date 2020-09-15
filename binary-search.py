2e64b72111fbcad7c2a153ca7ad77dd3493a9163
Eduard <mecozma@gmail.com>
2020-09-15 19:40:42 +0100

    Commented out the binary search function's code

--- binary-search.py
+++ binary-search.py
@@ -1,36 +1,41 @@
 def binary_search(numbers, item):
-  start = 0
-  end = len(numbers) - 1
+    """ It returns the index of x in a given array.
+    Args:
+        numbers: type array.
+        item: type integer.
+    Returns:
+        An integer.
+    """
+    if len(numbers) == 0:
+        return "Please provide an array"
 
-  while start <= end:
-    middle = (start + end) // 2
-    guess = numbers[middle]
-    if item == guess:
-      return middle
-    elif item > guess:
-      start = middle + 1
-    else:
-      end = middle - 1
-  return None
-  
+    start = 0
+    end = len(numbers) - 1
+
+    while start <= end:
+      middle = (start + end) // 2
+      guess = numbers[middle]
+      # Checks if the item is pressent at mid.
+      if item == guess:
+        return middle
+      # If item is greater, ignore left half.
+      elif item > guess:
+        start = middle + 1
+      # If item is smaller, ignore right half.
+      else:
+        end = middle - 1
+    return None
+
+# Test list.
 numbers_array = [1, 2, 3, 4, 5, 7, 8, 9, 10]
 
-print(binary_search(numbers_array, 10))
+# Number to identify.
+wally = 4
 
+# Function call
+result = binary_search(numbers_array, wally)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+if result == None:
+    print("Number is not present in the array")
+else:
+    print(f"The element is present at index: {result}")
